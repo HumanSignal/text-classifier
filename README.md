b2cc3f9456c8a2bef8647006bcba8070671383e5
nik <nik@heartex.net>
2019-11-29 22:13:13 +0200

    update readme & comments

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # text-classifier
 Tensorflow-based text classifier that could be integrated with Heartex/Label Studio
+
+# Usage
+```bash
+docker-compose up
+```
--- wsgi.py
+++ wsgi.py
@@ -5,10 +5,15 @@ from tfhub_classifier import TFHubClassifier
 from htx import app, init_model_server
 
 init_model_server(
+    # refer to class definition that inherits htx.BaseModel and implements load() and predict() methods
     create_model_func=TFHubClassifier,
+    # training script that will be called within RQ
     train_script=TFHubClassifier.fit_single_label,
+    # name of the Redis queue
     redis_queue=os.environ.get('RQ_QUEUE_NAME', 'default'),
+    # Redis host
     redis_host=os.environ.get('REDIS_HOST', 'localhost'),
+    # here we pass the kwargs parameters to train script
     tfhub_module_spec=os.environ.get('tfhub_module_spec', 'https://tfhub.dev/google/universal-sentence-encoder/2')
 )
 
